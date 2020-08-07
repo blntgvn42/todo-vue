b9bcb2ae697c44271e80f5236145e21998ff6c17
Bülent Güven <bulentguven97@hotmail.com>
2020-08-07 11:34:55 +0300

    completed

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,65 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos: [
+      { id: 1, todo: "todo 1", completed: false },
+      { id: 2, todo: "todo 2", completed: true },
+      { id: 3, todo: "todo 3", completed: false },
+      { id: 4, todo: "todo 4", completed: true },
+      { id: 5, todo: "todo 5", completed: false },
+      { id: 6, todo: "todo 6", completed: false },
+      { id: 7, todo: "todo 7", completed: true },
+      { id: 8, todo: "todo 8", completed: false },
+      { id: 9, todo: "todo 9", completed: true },
+      { id: 10, todo: "todo 10", completed: false },
+    ]
   },
   mutations: {
+    completeTodo(state, payload) {
+      state.todos.filter(todo => todo.id === payload.id)[0].completed = true
+    },
+    uncompleteTodo(state, payload) {
+      state.todos.filter(todo => todo.id === payload.id)[0].completed = false
+    },
+    deleteTodo(state, payload) {
+      state.todos = state.todos.filter(todo => todo.id !== payload.id)
+    },
+    changeId(state) {
+      let counter = 1;
+      state.todos.forEach(todo => {
+        todo.id = counter;
+        counter++;
+      })
+    },
+    createTodo(state, payload) {
+      state.todos = [...state.todos, { id: state.todos.length + 1, todo: payload.todo, completed: payload.completed }]
+    }
+  },
+  getters: {
+    allTodos: state => {
+      return state.todos
+    },
+    completedTodos: state => {
+      return state.todos.filter(todo => todo.completed)
+    },
+    completedTodosCount: (state, getters) => {
+      return getters.completedTodos.length
+    }
   },
   actions: {
+    todoCompleted({ commit }, todo) {
+      commit('completeTodo', { id: todo.id })
+    },
+    todoUncompleted({ commit }, todo) {
+      commit('uncompleteTodo', { id: todo.id })
+    },
+    todoDeleted({ commit }, todo) {
+      commit('deleteTodo', { id: todo.id })
+      commit('changeId');
+    },
+    todoCreated({ commit }, todo) {
+      commit('createTodo', { todo: todo.todo, completed: todo.completed === "Completed" ? true : false })
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,115 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="d-flex justify-space-around">
+      <v-dialog v-model="dialog" width="500">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">New Todo</v-btn>
+        </template>
+        <v-form @submit="submitTodo">
+          <v-card>
+            <v-card-title class="headline grey lighten-2">New Todo</v-card-title>
+            <v-card-text>
+              <v-text-field v-model="formData.todo" label="Type todo" required></v-text-field>
+              <v-radio-group v-model="formData.completed">
+                <v-radio
+                  v-for="operation in todoOperations"
+                  :key="operation"
+                  :label="operation"
+                  :value="operation"
+                ></v-radio>
+              </v-radio-group>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn
+                block
+                class="blue text--white text-lighten-1"
+                text
+                @click="submitTodo(); dialog = false;"
+              >I accept</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-form>
+      </v-dialog>
+      <v-btn @click="toggleTable">Show Todos</v-btn>
+    </div>
+    <v-simple-table v-if="showTable">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-center">Id</th>
+            <th class="text-center">Todo</th>
+            <th class="text-center">Is completed ?</th>
+            <th class="text-center">Operations</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="todo in allTodos"
+            :key="todo.id"
+            class="white--text text-lighten-1"
+            :class="todo.completed ? 'success' : 'warning'"
+          >
+            <td>{{ todo.id }}</td>
+            <td>{{ todo.todo }}</td>
+            <td>{{ todo.completed ? "Completed" : "Not Completed" }}</td>
+            <td class="d-flex justify-space-around align-center">
+              <v-btn small @click="todoCompleted(todo)">Mark as Completed</v-btn>
+              <v-btn small @click="todoUncompleted(todo)">Mark as Uncompleted</v-btn>
+              <v-btn small @click="todoDeleted(todo)">Delete Todo</v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapActions } from "vuex";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      todos: [],
+      todoOperations: ["Completed", "Uncompleted"],
+      dialog: false,
+      showTable: true,
+      formData: {
+        todo: "",
+        completed: "",
+      },
+    };
+  },
+  methods: {
+    ...mapActions([
+      "todoCreated",
+      "todoCompleted",
+      "todoUncompleted",
+      "todoDeleted",
+    ]),
+    toggleTable() {
+      this.showTable = !this.showTable;
+    },
+    submitTodo() {
+      this.todoCreated(this.formData);
+      this.formData.todo = "";
+      this.formData.completed = "";
+    },
+  },
+  computed: {
+    allTodos() {
+      return this.$store.getters.allTodos;
+    },
+    completedTodos() {
+      return this.$store.getters.completedTodos;
+    },
+    completedTodosCount() {
+      return this.$store.getters.completedTodosCount;
+    },
+  },
+  watch: {},
+};
 </script>
+
+<style scoped >
+</style>
